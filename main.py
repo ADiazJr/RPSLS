d605bed6356ff08ecdb1c1bb44aa5e88837e0cf7
unknown <arturodiazmay@gmail.com>
2022-08-29 11:45:24 -0500

    docs. algorithm for RPSLS project

---
+++ main.py
@@ -0,0 +1,8 @@
+# Step 1: Introduction to game
+# Step 2: choose if it's single player or multiplayer
+# Step 3: choose your gesture
+# Step 4: compare the gestures against one another
+# Step 5: determine which gesture wins
+# Step 6: play until someone wins twice
+# Step 7: Say who ends up winning the best of three
+
