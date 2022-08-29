b203711c45c5e76c86b124d238530107857a5a50
Lucy Ruiz <lucyruiz846@gmail.com>
2022-08-29 11:34:27 -0700

    doc: Session algorithm and classes

--- main.py
+++ main.py
@@ -7,3 +7,21 @@
 # Step 7: Say who ends up winning the best of three
 
 # Classes: greeting(), number_of_players(), battle_phase(), determine_winner()
+
+#Session info
+# Step 1. Display welcome
+# Step 2. Introduction to game
+# Step 3. Choose if it's single player or multiplayer
+# Step 4. Choose number of rounds
+# Step 5. Player one chooses gesture
+# Step 6. Player two chooses gesture
+# Step 7. Compare the gestures against one another
+# Step 8. Determine which gesture wins
+# Step 9. Play until someone wins twice
+# Step 10. Display winner who ends up winning the best of three
+
+# Classes: 
+# player(parent)
+#   - computer(child)
+#   - human(child)
+# game 
